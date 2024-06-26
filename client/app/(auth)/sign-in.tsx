cf6aa0fac5866d70a7f0cc7ff90d5662e525c3a3
Petar Vladov <60221587+petar1905@users.noreply.github.com>
2024-06-26 09:46:35 +0300

    Подобрени Login/Register страници

--- client/app/(auth)/sign-in.tsx
+++ client/app/(auth)/sign-in.tsx
@@ -64,8 +64,10 @@ export default function TabOneScreen() {
             setState={setPasswordVisibility}
             />}
           />
-          <TouchableOpacity style={styles.button} onPress={handleSubmit}><Text>SIGN IN</Text></TouchableOpacity>
-          <Link href="(auth)/sign-up">Here</Link>
+          <TouchableOpacity style={styles.button} onPress={handleSubmit}>
+            <Text style={styles.buttonText}>SIGN IN</Text>
+          </TouchableOpacity>
+          <Link href="(auth)/sign-up">No account? Sign up here!</Link>
         </View>
         </View>
        
@@ -133,4 +135,7 @@ const styles = StyleSheet.create({
     paddingHorizontal: 10,
     backgroundColor: "transparent"
   },
+  buttonText: {
+    color: "white"
+  }
 });
